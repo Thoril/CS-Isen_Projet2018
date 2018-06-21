203bfffc74dc75b696bb6eb169930f3a6a694df1
Unknown <artru.thomas.monchal@gmail.com>
2018-06-21 11:08:06 +0200

    Question  2

---
+++ Isen.Artru/Isen.Artru.Library/INode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Isen.Artru.Library
+{
+    public interface INode
+    {
+        string value { get; set; }
+        Guid id { get; }
+        Node parent { get; set; }
+        List<Node> children { get; set; }
+        int depth { get; }
+        
+    }
+}
